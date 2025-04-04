81d70d006edf2a2f8bc82e19a062b4908546e274
alessandrofalcoo <alefalco.1003@gmail.com>
2025-04-04 12:58:47 +0200

    feat: implement Header and Main components; update App to include movie search functionality and Bootstrap styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
+  const [searchText, setSearchText] = useState('')
   const api_key = import.meta.env.VITE_MOVIE_DB_API_KEY;
-  const base_movies_api_url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${serchText}`
+  const base_movies_api_url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${searchText}`
+
+
   fetch(base_movies_api_url)
     .then(res => res.json())
     .then(data => {
       console.log(data);
+      setSearchText(searchText)
 
     })
 
+
+
   return (
     <>
-
+      <Header />
+      <Main />
     </>
   )
 }
