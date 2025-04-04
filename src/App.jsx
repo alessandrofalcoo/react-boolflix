6e47dbcfe1e655778a1a342e33784d420251e121
alessandrofalcoo <alefalco.1003@gmail.com>
2025-04-04 16:59:56 +0200

    feat: integrate CountContext to manage search state and API key; update App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import { useEffect, useState } from 'react'
 import Header from './components/Header';
 import Main from './components/Main';
+import CountContext from './contexts/CountContexts';
 
 function App() {
   const [searchText, setSearchText] = useState('')
   const api_key = import.meta.env.VITE_MOVIE_DB_API_KEY;
+  console.log(api_key);
+
   const base_movies_api_url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${searchText}`
+  console.log(base_movies_api_url);
 
 
-  fetch(base_movies_api_url)
+
+  const fetchedData = fetch(base_movies_api_url)
     .then(res => res.json())
     .then(data => {
       console.log(data);
-      setSearchText(searchText)
+
+      setSearchText()
 
     })
 
@@ -20,8 +26,10 @@ function App() {
 
   return (
     <>
-      <Header />
-      <Main />
+      <CountContext.Provider value={{ searchText, api_key, base_movies_api_url, fetchedData }}>
+        <Header />
+        <Main />
+      </CountContext.Provider>
     </>
   )
 }
