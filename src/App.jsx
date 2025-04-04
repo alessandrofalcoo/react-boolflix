8ad8a91dfbfc4b57a38dc14031f21e585a93c727
alessandrofalcoo <alefalco.1003@gmail.com>
2025-04-04 12:26:58 +0200

    refactor: update environment variable naming and integrate movie API fetch; add Header, Main, and CountContext components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import { useState } from 'react'
 
 function App() {
+  const api_key = import.meta.env.VITE_MOVIE_DB_API_KEY;
+  const base_movies_api_url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${serchText}`
+  fetch(base_movies_api_url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+
+    })
 
   return (
     <>
