9dbb70c442bf97726c607be7d87a1f139000ca28
alessandrofalcoo <alefalco.1003@gmail.com>
2025-04-07 12:35:25 +0200

    refactor: update Main component to use backdrop images and improve card layout

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,16 +6,18 @@ export default function Main() {
 
     return (
         <>
-            {movies.map(movie => (
-                <div className="card" key={movie.id}>
-                    <img className="card-img-top" src={movie.poster_path} />
-                    <div className="card-body">
-                        <h4 className="card-title">{movie.title}</h4>
-                        <p className="card-text">{movie.overview}</p>
+            <div className="container my-3">
+                {movies.map(movie => (
+                    <div className="card my-3" key={movie.id}>
+                        <img className="card-img-top" src={movie.backdrop_path} />
+                        <div className="card-body">
+                            <h4 className="card-title">{movie.title}</h4>
+                            <p className="card-text">{movie.overview}</p>
+                            <p className="card-language text-uppercase fw-bold">{movie.original_language}</p>
+                        </div>
                     </div>
-                </div>
-
-            ))}
+                ))}
+            </div>
         </>
     )
 }
