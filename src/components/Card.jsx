09a38f2b7118c269ec21176abadd9cec47534981
alessandrofalcoo <alefalco.1003@gmail.com>
2025-04-07 16:53:46 +0200

    feat: enhance Card component with movie overview and improved hover effects

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,11 +12,12 @@ export default function Card() {
                         {movies.map(movie => (
                             <div className="card text-white" key={movie.id}>
                                 <img className="card-img-top" src={movie.poster_path === null ? '../public/img/Netflix-symbol.png' : `http://image.tmdb.org/t/p/w342/${movie.poster_path}`} alt={movie.title} />
-                                <div className="card-body d-none">
+                                <div className="card-body">
                                     <h4 className="card-title">Movie: {movie.title}</h4>
-                                    <p className="card-text">Original title: {movie.original_title}</p>
-                                    <p className="card-language">Language: <strong className="text-uppercase">{movie.original_language}</strong></p>
-                                    <p className="card-rating">Rating: {movie.vote_average}</p>
+                                    <p className="card-text"><strong>Original title:</strong> {movie.original_title}</p>
+                                    <p className="card-language"><strong>Language:</strong> <strong className="text-uppercase">{movie.original_language}</strong></p>
+                                    <p className="card-rating"><strong>Rating:</strong> {movie.vote_average}</p>
+                                    <p className="card-overview"><strong>Overview:</strong> {movie.overview}</p>
                                 </div>
                             </div>
                         ))}
