019fdaeb70ec0eb038a9c3a8669e583d0f3c5cdf
alessandrofalcoo <alefalco.1003@gmail.com>
2025-04-07 15:30:08 +0200

    feat: add Netflix logo to Header component and update Card layout for better styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,12 +9,12 @@ export default function Header() {
     return (
         <>
             <nav
-                className="navbar navbar-expand-sm navbar-light bg-light"
+                className="navbar navbar-expand-sm navbar-light "
             >
                 <div className="container">
-                    <a className="navbar-brand" href="#">LOGO</a>
+                    <a className="navbar-brand" href="#"><img className="img-fluid logo" src="../public/img/Netflix-symbol.png"></img></a>
                     <button
-                        className="navbar-toggler d-lg-none"
+                        className="navbar-toggler"
                         type="button"
                         data-bs-toggle="collapse"
                         data-bs-target="#collapsibleNavId"
@@ -24,18 +24,7 @@ export default function Header() {
                     >
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="collapsibleNavId">
-                        <ul className="navbar-nav me-auto mt-2 mt-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" href="#" aria-current="page"
-                                >Home
-                                    <span className="visually-hidden">(current)</span></a
-                                >
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
-                            </li>
-                        </ul>
+                    <div className="collapse navbar-collapse justify-content-end" id="collapsibleNavId">
                         <form onSubmit={(e) => { e.preventDefault(); fetchData(); }} className="d-flex my-2 my-lg-0">
                             <input
                                 className="form-control me-sm-2"
