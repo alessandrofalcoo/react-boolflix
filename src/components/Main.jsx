7d695a30326d20b6a1bbd141ebc9e9a9816bd2d7
alessandrofalcoo <alefalco.1003@gmail.com>
2025-04-07 12:47:17 +0200

    feat: update Main component to display movie posters and original titles in card layout

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,11 +9,12 @@ export default function Main() {
             <div className="container my-3">
                 {movies.map(movie => (
                     <div className="card my-3" key={movie.id}>
-                        <img className="card-img-top" src={movie.backdrop_path} />
+                        <img className="card-img-top " src={`http://image.tmdb.org/t/p/w1920/${movie.poster_path}`} alt={movie.title} />
                         <div className="card-body">
                             <h4 className="card-title">{movie.title}</h4>
-                            <p className="card-text">{movie.overview}</p>
+                            <p className="card-text">{movie.original_title}</p>
                             <p className="card-language text-uppercase fw-bold">{movie.original_language}</p>
+                            <p className="card-rating">{movie.vote_average}</p>
                         </div>
                     </div>
                 ))}
